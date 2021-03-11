2d2b5c62a6e43b1c9f70a860d07178adcef9d1a6
Michael Bakshi <michael.bakshi83@gmail.com>
2021-03-11 19:56:55 +0200

    Add project files.

---
+++ 07.03.38/MyFileManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07._03._38
+{
+    public class MyFileManager
+    {
+        public const string FILE_PATH = @"c:\final.txt";
+
+        private MyFileManager()
+        {
+
+        }
+
+        public async static void ReadFromFile()
+        {
+            await Task.Run(() =>
+            {
+                string mytext = File.ReadAllText(@"c:\final.txt");
+                Console.WriteLine(mytext);
+            });
+            
+        }
+    }
+}
---
+++ 07.03.38/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07._03._38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => MyFileManager.ReadFromFile());
+            t1.Start();
+            Thread.Sleep(1000);
+        }
+
+    }
+
+
+}
